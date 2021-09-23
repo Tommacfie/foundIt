41e99794cbf46ccb4f8aae8090b7ff79ab3aed0d
Tommacfie <tommacfie@aol.com>
2021-09-23 16:12:08 +0200

    feat: implement item controller getItems
    Signed-off-by: Tommacfie <tommacfie@aol.com>

--- server/controllers/item.controller.js
+++ server/controllers/item.controller.js
@@ -1,25 +1,36 @@
 const Item = require('../models/item.model');
 
-exports.getItems = async () => {
+exports.getItems = async (req, res) => {
   try {
-
+    console.log(req.body, 'items GET');
+    const items = await Item.find();
+    console.log(items, 'items GET');
+    res.send(items);
+    res.status(200);
   } catch (error) {
-
+    res.send(error);
+    res.status(500);
   }
 };
 
-exports.postItem = async () => {
+exports.postItem = async (req, res) => {
   try {
+    console.log(req.body, 'items POST');
+    const { title, description, brandModel, image, where, lostFound, submittedBy } = req.body;
+
 
   } catch (error) {
-
+    res.send(error);
+    res.status(500);
   }
 };
 
-exports.deleteItem = async () => {
+exports.deleteItem = async (req, res) => {
   try {
+    console.log(req.body, 'items DELETE');
 
   } catch (error) {
-
+    res.send(error);
+    res.status(500);
   }
 };
