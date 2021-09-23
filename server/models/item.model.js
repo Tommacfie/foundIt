ecc34d2392d27082ffae87c9190a20922d79b4b8
Tommacfie <tommacfie@aol.com>
2021-09-23 15:43:46 +0200

    chore: implement user and item models
    Signed-off-by: Tommacfie <tommacfie@aol.com>

---
+++ server/models/item.model.js
@@ -0,0 +1,39 @@
+const mongoose = require('./index');
+
+const Schema = mongoose.Schema();
+
+const itemModel = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  brandModel: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: false,
+    default: 'No image provided'
+  },
+  whereFound: {
+    type: String,
+    required: true,
+  },
+  lostFound: {
+    type: Boolean,
+    required: true,
+  },
+  submitted_by: {
+    type: String,
+    required: true
+  }
+});
+
+const Item = mongoose.model('Item', itemModel);
+
+module.exports = Item;
---
+++ server/models/user.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('./index');
+
+const Schema = mongoose.Schema();
+
+const userModel = new Schema({
+  first_name: {
+    type: String,
+    required: true
+  },
+  last_name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const User = mongoose.model('User', userModel);
+
+module.export = User;
