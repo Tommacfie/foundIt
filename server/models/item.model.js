2ce39518aaa0abc97ed612c050236d62ed01151f
Tommacfie <tommacfie@aol.com>
2021-09-23 16:05:52 +0200

    chore: implement routes
    Signed-off-by: Tommacfie <tommacfie@aol.com>

--- server/models/item.model.js
+++ server/models/item.model.js
@@ -1,6 +1,6 @@
 const mongoose = require('./index');
 
-const Schema = mongoose.Schema();
+const Schema = mongoose.Schema;
 
 const itemModel = new Schema({
   title: {
--- server/router.js
+++ server/router.js
@@ -1,3 +1,11 @@
 const router = require('express').Router();
+const itemController = require('./controllers/item.controller');
+const userController = require('./controllers/user.controller');
+
+router.get('/items', itemController.getItems);
+router.post('/items', itemController.postItem);
+router.delete('/items', itemController.deleteItem);
+router.post('/login', userController.loginUser);
+router.post('/register', userController.createUser);
 
 module.exports = router;
