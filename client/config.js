112b6895e0f75286e3aa85c63d4eaf5a94245163
Tommacfie <tommacfie@aol.com>
2021-09-24 17:06:49 +0200

    fix: obscure .env variables
    Signed-off-by: Tommacfie <tommacfie@aol.com>

--- client/config.js
+++ client/config.js
@@ -1,8 +1,7 @@
-import dotenv from 'dotenv';
-dotenv.config();
+const Config = {};
 
-const server = process.env.SERVER || 'http://localhost:3001';
-
+Config.server = 'http://localhost:3001';
+Config.token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MTRjYTdhZDc5NjI5NTA3YThiNjMwM2YiLCJpYXQiOjE2MzI0OTEwOTJ9.1QBvUMfsnjB4osRo2ixJmH0vPxoVW_N25ZDW17iqPBA'
 // console.log(server, 'server');//DELETEME
 
-export default server;
+export default Config;
--- client/services/api.service.js
+++ client/services/api.service.js
@@ -1,26 +1,26 @@
 import React from 'react-native';
-import server from '../config';
-
+import Config from '../config';
 const Api = {};
 
 const login = async () => { }
 const register = async () => { }
 
 Api.getItems = async () => {
-  const items = await fetch(`${server}/items`, {
+  const response = await fetch(`${Config.server}/items`, {
     headers: {
-      authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MTRjYTdhZDc5NjI5NTA3YThiNjMwM2YiLCJpYXQiOjE2MzI0OTEwOTJ9.1QBvUMfsnjB4osRo2ixJmH0vPxoVW_N25ZDW17iqPBA'
+      authorization: Config.token
     }
   })
-  return items.json();
+  const items = await response.json();
+  return items;
 };
 
 Api.postItem = async (item) => {
-  const response = await fetch(`${server}/items`, {
+  const response = await fetch(`${Config.server}/items`, {
     method: 'POST',
     headers: {
       'content-type': 'application/json',
-      authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MTRjYTdhZDc5NjI5NTA3YThiNjMwM2YiLCJpYXQiOjE2MzI0OTEwOTJ9.1QBvUMfsnjB4osRo2ixJmH0vPxoVW_N25ZDW17iqPBA'
+      authorization: Config.token
     },
     body: JSON.stringify(item)
   })
@@ -29,11 +29,11 @@ Api.postItem = async (item) => {
 };
 
 Api.deleteItem = async (id) => {
-  const response = await fetch(`${server}/items`, {
+  const response = await fetch(`${Config.server}/items`, {
     method: 'DELETE',
     headers: {
       'content-type': 'application/json',
-      authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MTRjYTdhZDc5NjI5NTA3YThiNjMwM2YiLCJpYXQiOjE2MzI0OTEwOTJ9.1QBvUMfsnjB4osRo2ixJmH0vPxoVW_N25ZDW17iqPBA'
+      authorization: Config.token
     },
     body: JSON.stringify({ _id: id })
   })
