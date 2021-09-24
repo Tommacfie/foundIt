5a53aa15c1d377d0367aa1672b81d54249c469d7
Tommacfie <tommacfie@aol.com>
2021-09-24 15:16:31 +0200

    chore: create screen components
    Signed-off-by: Tommacfie <tommacfie@aol.com>

---
+++ client/services/api.service.js
@@ -0,0 +1,8 @@
+import React from 'react-native';
+
+const login = async () => { }
+const getItems = async () => { }
+const register = async () => { }
+
+const postItem = async () => { }
+const deleteItem = async () => { }
