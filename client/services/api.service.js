5e228514f3569494fb53f0c9d1c7f77e87d43daa
Tommacfie <tommacfie@aol.com>
2021-09-24 16:28:02 +0200

    feat: implement Api DELETE for './items route
    Signed-off-by: Tommacfie <tommacfie@aol.com>

--- client/services/api.service.js
+++ client/services/api.service.js
@@ -7,7 +7,6 @@ const login = async () => { }
 const register = async () => { }
 
 Api.getItems = async () => {
-  // console.log(server, 'server');
   const items = await fetch(`${server}/items`, {
     headers: {
       authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MTRjYTdhZDc5NjI5NTA3YThiNjMwM2YiLCJpYXQiOjE2MzI0OTEwOTJ9.1QBvUMfsnjB4osRo2ixJmH0vPxoVW_N25ZDW17iqPBA'
@@ -29,8 +28,17 @@ Api.postItem = async (item) => {
   return postedItem;
 };
 
-const deleteItem = async () => {
+Api.deleteItem = async (id) => {
+  const response = await fetch(`${server}/items`, {
+    method: 'DELETE',
+    headers: {
+      'content-type': 'application/json',
+      authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MTRjYTdhZDc5NjI5NTA3YThiNjMwM2YiLCJpYXQiOjE2MzI0OTEwOTJ9.1QBvUMfsnjB4osRo2ixJmH0vPxoVW_N25ZDW17iqPBA'
+    },
+    body: JSON.stringify({ _id: id })
+  })
+  const deletedItem = await response.json();
+  return deletedItem;
+};
 
-
-}
 export default Api;
