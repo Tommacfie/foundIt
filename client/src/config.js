83d1feb88c7eebfa436446f0e06993cd53d43881
Tommacfie <tommacfie@aol.com>
2021-09-26 09:01:55 +0200

    fix: ignore config file
    Signed-off-by: Tommacfie <tommacfie@aol.com>

--- client/src/config.js
+++
@@ -1,7 +0,0 @@
-const Config = {};
-
-Config.server = 'http://localhost:3001';
-Config.token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MTRjYTdhZDc5NjI5NTA3YThiNjMwM2YiLCJpYXQiOjE2MzI2Mzg1NTV9.WEC63b-10lFIsUILSZegYsN6Hvy0eJBPljX9UEy0DF4'
-// console.log(server, 'server');//DELETEME
-
-export default Config;
