8a5c62a973972cc4f8f73d319b5a156c9849eeee
Tommacfie <tommacfie@aol.com>
2021-10-25 09:19:07 +0200

    fix: environment variable names
    Signed-off-by: Tommacfie <tommacfie@aol.com>

--- client/src/config.js
+++ client/src/config.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 const Config = {};
 
-Config.server = process.env.REACT_APP_CLIENTSERVER
+Config.server = process.env.REACT_APP_DATABASESERVER
 Config.imageServer = process.env.REACT_APP_IMAGESERVER;
 
 export default Config;
