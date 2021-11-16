cd77645b1792e21a16ddd0ab05024bfcdf63d3aa
Tommacfie <tommacfie@aol.com>
2021-11-16 10:46:55 +0100

    feat: close database after dropping with script
    Signed-off-by: Tommacfie <tommacfie@aol.com>

--- server/utils/drop.db.js
+++ server/utils/drop.db.js
@@ -1,3 +1,10 @@
 const mongoose = require("../models/index");
 
-mongoose.connection.dropDatabase();
+mongoose.connection
+  .dropDatabase()
+  .then(() => {
+    console.log("Mock data dropped from database");
+  })
+  .then(() => {
+    mongoose.connection.close();
+  });
