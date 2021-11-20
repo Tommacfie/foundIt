184926445ac2e1b350f6328566f819c44dfc9f3c
Tommacfie <tommacfie@aol.com>
2021-11-20 13:10:47 +0100

    feat: add error handling to api service
    Signed-off-by: Tommacfie <tommacfie@aol.com>

--- client/src/services/api.service.js
+++ client/src/services/api.service.js
@@ -4,63 +4,88 @@ import Config from "../config";
 const Api = {};
 
 Api.login = async (userData) => {
-  const response = await fetch(`${Config.server}/login`, {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-    },
-    body: JSON.stringify(userData),
-  });
-  const loginSuccess = await response.json();
-  return loginSuccess;
+  try {
+    const response = await fetch(`${Config.server}/login`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+    const loginSuccess = await response.json();
+    return loginSuccess;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
 };
 
 Api.register = async (userData) => {
-  const response = await fetch(`${Config.server}/register`, {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-    },
-    body: JSON.stringify(userData),
-  });
-  const newUser = await response.json();
-  return newUser;
+  try {
+    const response = await fetch(`${Config.server}/register`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+    const newUser = await response.json();
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
 };
 
 Api.getItems = async (authorisation) => {
-  const response = await fetch(`${Config.server}/items`, {
-    headers: {
-      authorization: `Bearer ${authorisation}`,
-    },
-  });
-  const items = await response.json();
-  return items;
+  try {
+    const response = await fetch(`${Config.server}/items`, {
+      headers: {
+        authorization: `Bearer ${authorisation}`,
+      },
+    });
+    const items = await response.json();
+    return items;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
 };
 
 Api.postItem = async (item, authorisation) => {
-  const response = await fetch(`${Config.server}/items`, {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-      authorization: `Bearer ${authorisation}`,
-    },
-    body: JSON.stringify(item),
-  });
-  const postedItem = await response.json();
-  return postedItem;
+  try {
+    const response = await fetch(`${Config.server}/items`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        authorization: `Bearer ${authorisation}`,
+      },
+      body: JSON.stringify(item),
+    });
+    const postedItem = await response.json();
+    return postedItem;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
 };
 
 Api.deleteItem = async (id, authorisation) => {
-  const response = await fetch(`${Config.server}/items`, {
-    method: "DELETE",
-    headers: {
-      "content-type": "application/json",
-      authorization: `Bearer ${authorisation}`,
-    },
-    body: JSON.stringify({ _id: id }),
-  });
-  const deletedItem = await response.json();
-  return deletedItem;
+  try {
+    const response = await fetch(`${Config.server}/items`, {
+      method: "DELETE",
+      headers: {
+        "content-type": "application/json",
+        authorization: `Bearer ${authorisation}`,
+      },
+      body: JSON.stringify({ _id: id }),
+    });
+    const deletedItem = await response.json();
+    return deletedItem;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
 };
 
 Api.postImage = async (image) => {
@@ -68,6 +93,7 @@ Api.postImage = async (image) => {
     const response = await Axios.post(Config.imageServer, image);
     return response;
   } catch (error) {
+    console.log(error);
     return error;
   }
 };
