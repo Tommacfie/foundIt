e132862eac8613ed559a103ef92c7aabeefb8525
Tommacfie <tommacfie@aol.com>
2021-11-23 10:07:46 +0100

    chore: remove header component from scss
    Signed-off-by: Tommacfie <tommacfie@aol.com>

--- client/src/components/presentational/logo.component.js
+++ client/src/components/presentational/logo.component.js
@@ -3,10 +3,10 @@ import LogoIcon from "../../assets/magnifying-glass.svg";
 
 const Logo = () => {
   return (
-    <h1 className="logo">
-      FoundIt
+    <div className="logo">
+      <h1 className="logo__text">FoundIt</h1>
       <img className="logo__icon" src={LogoIcon} alt="magnifying-glass-icon" />
-    </h1>
+    </div>
   );
 };
 
